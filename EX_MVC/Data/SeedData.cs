651242288d6a854a1ecfd182faeeebb196a45f85
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2022-12-17 10:42:52 +0700

    Add project files.

---
+++ EX_MVC/Data/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using EX_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EX_MVC.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new EX_MVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<EX_MVCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Employee.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Employee.AddRange(
+                    new Employee
+                    {
+                        EmployeeId = "EM001",
+                        EmployeeName = "John Carter",
+                        Department = "HR",
+                        Salary = 3000
+                    },
+                    new Employee
+                    {
+                        EmployeeId = "EM002",
+                        EmployeeName = "Michael Bean",
+                        Department = "SC",
+                        Salary = 1300
+                    },
+                    new Employee
+                    {
+                        EmployeeId = "EM003",
+                        EmployeeName = "Jimmy Floy",
+                        Department = "MD",
+                        Salary = 2000
+                    },
+                    new Employee
+                    {
+                        EmployeeId = "EM004",
+                        EmployeeName = "Mary Brown",
+                        Department = "MD",
+                        Salary = 2000
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
